31f54ee0e508ca3df7eff4cb7949fb58d2df4d09
MOSapeizer <k471352@gmail.com>
2016-04-26 11:14:37 +0800

    packeting

--- src/sosconnector/Main.java
+++ src/sosconnector/Main.java
@@ -1,11 +1,6 @@
 package sosconnector;
 
-import sosconnector.GovConfigure.Configure;
-
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
 import java.util.Date;
 import java.util.Timer;
 
@@ -18,17 +13,17 @@ public class Main {
     private final static int minutes = 60 * seconds;
 
     public static void main(String[] args) throws JAXBException {
-//        Timer timer = new Timer( true );
-//        timer.schedule(new Twed(), 5 * seconds, 10 * minutes);
-//        System.out.println("現在時間：" + new Date());
-//
-//        try {
-//            Thread.sleep(120 * seconds);
-//        } catch (InterruptedException ignored) {
-//        }
-//
-//        System.out.println("結束時間：" + new Date());
-//        timer.cancel();
+        Timer timer = new Timer( true );
+        timer.schedule(new Twed(), 5 * seconds, 10 * minutes);
+        System.out.println("現在時間：" + new Date());
+
+        try {
+            Thread.sleep(120 * seconds);
+        } catch (InterruptedException ignored) {
+        }
+
+        System.out.println("結束時間：" + new Date());
+        timer.cancel();
 
     }
 }
