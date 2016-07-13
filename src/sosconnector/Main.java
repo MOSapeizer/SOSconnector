ba9ac0141a885e6696576c666eab61430ae6bed1
MOSapeizer <k471352@gmail.com>
2016-07-13 09:26:49 +0800

    Change project xml file path

--- src/sosconnector/Main.java
+++ src/sosconnector/Main.java
@@ -14,7 +14,7 @@ import java.util.Timer;
 public class Main {
 
 //    public static final String configures_path = "src/sosconnector/Configure/";
-    public static final String configures_path = "./xml/";
+    public static final String configures_path = "/usr/local/sos-connector/xml/";
     public static final int start_buff = 5 * 1000;
 
     public static void main(String[] args) throws JAXBException, NotDirectoryException {
